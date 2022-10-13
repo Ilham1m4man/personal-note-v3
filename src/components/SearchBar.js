581cb109beac6f1fdfa911534a9a1d6a3a78377e
Ilham1m4man <kucingdiki@gmail.com>
2022-10-14 01:03:13 +0700

    Add search bar UI component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import LocaleContext from '../contexts/LocaleContext';
+
+function SearchBar({ keyword, keywordChange }) {
+    const { locale } = React.useContext(LocaleContext);
+    return (
+        <div className="search-bar">
+            <input
+                type="text"
+                placeholder={locale === 'id' ? 'Cari berdasarkan judul' : 'Search by title'}
+                value={keyword}
+                onChange={(event) => keywordChange(event.target.value)} />
+        </div>
+    )
+}
+
+SearchBar.propType = {
+    keyword: PropTypes.string.isRequired,
+    keywordChange: PropTypes.func.isRequired
+}
+
+export default SearchBar;
