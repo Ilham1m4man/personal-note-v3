fcf9ebffe36c85b75288d3577dbdd3e2a2b7c2e6
Ilham1m4man <kucingdiki@gmail.com>
2022-10-14 01:13:30 +0700

    Add path storage

---
+++ src/utils/path-name.js
@@ -0,0 +1,15 @@
+const error404Path = '*';
+const homePath = '/';
+const detailPath = '/detail/:id';
+const archivePath = '/archive';
+const createPath = '/create';
+const registerPath = '/register';
+
+export {
+    error404Path,
+    homePath,
+    detailPath,
+    archivePath,
+    createPath,
+    registerPath
+}
